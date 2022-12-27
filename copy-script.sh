8d8ec3396c3bf1256954ec3e10a8540976a2a555
Morten Victor Nordbye <74780083+Morten03Nordbye@users.noreply.github.com>
2022-12-27 12:12:31 +0100

    Update copy-script.sh

--- copy-script.sh
+++ copy-script.sh
@@ -5,7 +5,7 @@ src="/home/mnordbye"
 dst="/home/backup"
 
 # Exclude temporary files and directories
-exclude="--exclude=*.tmp --exclude=*.temp --exclude=tmp --exclude=temp"
+exclude="--exclude=~/.local --exclude=~/.mozilla"
 
 # Copy all files and directories, excluding temporary files and directories
 rsync -av --info=progress2 $exclude "$src" "$dst"
