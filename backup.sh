80500d6b65428d1492c3e66fb32939510d48d7fb
root <morten.victor.nordbye@basefarm-orange.com>
2023-03-17 14:36:42 +0100

    Added new version

--- backup.sh
+++ backup.sh
@@ -3,17 +3,24 @@
 # Copy part
 #------------
 mkdir -p /home/backup
+chown mnordbye:mnordbye /home/backup
+chmod 700 /home/backup
+
 # Set the source and destination directories
 copysrc="/home/mnordbye"
 copydst="/home/backup"
-# Exclude temporary files and directories
-exclude_list=(".local" ".mozilla" ".cache" ".steam" ".steampath" ".steampid" "Steam" ".wine" )
-exclude_options=""
-for pattern in "${exclude_list[@]}"; do
-  exclude_options+="--exclude=$pattern "
-done
+
+# Read the exclude list from a file
+exclude_list_file="/home/script/exclude_list.txt"
+exclude_options=()
+while IFS= read -r pattern
+do
+  exclude_options+=(--exclude="$pattern")
+done < "$exclude_list_file"
+
 # Copy all files and directories, excluding temporary files and directories
-rsync -av --info=progress2 $exclude_options "$copysrc" "$copydst"
+rsync -av --info=progress2 "${exclude_options[@]}" "$copysrc/" "$copydst"
+
 #------------
 # Zip part
 #------------
@@ -21,5 +28,12 @@ rsync -av --info=progress2 $exclude_options "$copysrc" "$copydst"
 zipsrc="/home/backup/"
 # Set the destination for the zip file
 zipdst="/home/backup_$(date +%F).zip"
+
 # Create a zip archive of the source directory and store it in the destination
 zip -r "$zipdst" "$zipsrc"
+
+# Change the owner of the zipped file to the user being backed up
+chown mnordbye:mnordbye "$zipdst"
+
+# Remove the backup directory
+rm -r "$copydst"
